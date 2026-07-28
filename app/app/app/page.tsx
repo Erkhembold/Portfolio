d5a79926886cb0713f588705ac98c883dc64fd3c
Erkhembold <nag06091@gmail.com>
2026-07-28 14:07:51 +0800

    Create page.tsx

---
+++ app/app/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from "@/components/Hero"
+import FilmPosters from "@/components/FilmPosters"
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <FilmPosters />
+    </main>
+  )
+}
